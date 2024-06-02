20b8133399b8b0d5e96283bdc4b723813bff2471
Plinio Zanini <39263081+pliniozanini@users.noreply.github.com>
2024-06-02 00:17:27 +0000

    feat: mock for new word generator

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import json
+import numpy as np
 
-st.text("testando new word generator")
+
+# read json file as a dictionary
+with open('new_words.json') as f:
+    data = json.load(f)
+
+# sample a key from the data dictionary in random fashion
+sample_key = np.random.choice(list(data.keys()))
+
+st.write(f"Word: {sample_key}")
+st.write(f"Meaning: {data[sample_key]}")
